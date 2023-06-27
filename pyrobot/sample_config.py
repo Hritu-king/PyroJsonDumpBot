b15abc89b974e63235b3ffd46f62c92f74dfd33e
Hritu-king <87654408+Hritu-king@users.noreply.github.com>
2023-06-27 12:31:45 +0530

    Update sample_config.py

--- pyrobot/sample_config.py
+++ pyrobot/sample_config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6181365570:AAEklmfU6eifF4wQt9EgueVI5tM0zDVHHRQ")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", None)
+    APP_ID = int(os.environ.get("APP_ID", 29616312))
+    API_HASH = os.environ.get("API_HASH", "dd1a05ab4c47a5a037cc067cf4bded27")
